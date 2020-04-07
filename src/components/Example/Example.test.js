bb2d648c68af2734278cd16690447e5f5f697725
ron nachmany <rnachmany@anyvision.co>
2020-04-07 16:00:38 +0300

    test example

---
+++ src/components/Example/Example.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import Example from './Example';
+
+
+describe('Example', () => {
+  it('CheckboxWithLabel changes the text after click', () => {
+    const {queryByLabelText, getByLabelText} = render(
+      <Example />,
+    )
+
+    expect(true).toBeTruthy();
+  })
+})
