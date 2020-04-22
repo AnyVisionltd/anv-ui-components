cc5c790bece6cff6a3b0fcde81edc719b8ad236d
matyanyvision <matym@anyvision.co>
2020-04-22 10:24:29 +0300

    PR + lint + tests fixes

--- src/utils/storybook/decorators.js
+++ src/utils/storybook/decorators.js
@@ -4,4 +4,10 @@ import styles from './decorators.module.scss'
 
 export const centerDecorator = (story) => <div className={ styles.center }>{ story() }</div>
 
+/**
+ * The following function was added to overcome a bug with Storyshot lib, that didn't allow stories with react hooks
+ * within them to load, the tests were created with errors.
+ * Please see the following thread: https://github.com/storybookjs/storybook/issues/8177
+ * Consider removing this line when the issue is fixed.
+ */
 addDecorator(createElement)
