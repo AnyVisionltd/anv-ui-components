722a4c0e6a6a8397a828d97f807747e593f588a7
ron nachmany <rnachmany@anyvision.co>
2020-05-31 18:58:07 +0300

    sort

--- src/components/Table/UseTableData/useTableData.js
+++ src/components/Table/UseTableData/useTableData.js
@@ -1,11 +1,12 @@
 import { useMemo, useContext, useCallback } from 'react'
+import { orderTypes } from "../../../utils/enums/common"
 import TableContext from '../TableContext'
 
 const useTableData = () => {
   const { state } = useContext(TableContext)
-  const { data, controlled, filters } = state
+  const { data, controlled, filters, sort } = state
 
-  const filterData = useCallback(() => {
+  const filterData = useCallback(data => {
     return data.filter(row => {
       return filters.every(({ field, value }) => {
         if(field) {
@@ -15,15 +16,32 @@ const useTableData = () => {
         }
       })
     })
-  }, [data, filters])
+  }, [filters])
+
+  const sortData = useCallback(data => {
+    if(!sort.sortBy.field) {
+      return data
+    }
+    const defaultSort = (data, field) => data.sort((rowA, rowB) => (
+      rowA[field].localeCompare(rowB[field], undefined, { sensitivity: 'base' }))
+    )
+    const { field, order } = sort.sortBy
+    const sortedData = defaultSort(data, field).reverse()
+    return order === orderTypes.DESC ? sortedData.reverse() : sortedData
+
+  }, [sort])
 
   return useMemo(() => {
+    let tableData
     if(controlled) {
       return data
     }
 
-    return filterData()
-  }, [data, controlled, filterData])
+    tableData = filterData(data)
+    tableData = sortData(data)
+
+    return tableData
+  }, [data, controlled, sortData, filterData])
 
 }
 
