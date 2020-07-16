c7e5ce8108a19f0a22d689e22d9d0de8fcf3758d
DShakedAnyVision <dshaked@anyvision.co>
2020-07-16 18:16:02 +0300

    #BTTRIBEV2-8318-input-chip #time 6h
    * Added ChipsInput
    * Added storybook for ChipsInput
    * No Tests

---
+++ src/components/ChipsInput/ChipsInput.stories.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { select, text } from '@storybook/addon-knobs'
+import ChipsInput from './ChipsInput'
+import { ReactComponent as EyeEnabled } from '../../assets/svg/EyeEnabled.svg'
+import { ReactComponent as ArrowSolidRight } from '../../assets/svg/ArrowSolidRight.svg'
+import { ReactComponent as Filter } from '../../assets/svg/Filter.svg'
+import { centerDecorator } from '../../utils/storybook/decorators'
+import styles from '../../styles/storybook/index.module.scss'
+import Button from "../Button/Button"
+
+const mockInitialData = [
+  { label: 'first' },
+  { label: 'second' },
+  { label: 'third with icon', icon: <ArrowSolidRight /> }
+]
+
+export default {
+  title: 'Components/ChipsInput',
+  component: ChipsInput,
+  decorators: [centerDecorator],
+}
+
+export const Default = () => (
+  <ChipsInput
+    onChange={ action('Result') }
+  />
+)
+
+export const initialValues = () => (
+  <ChipsInput
+    values={ mockInitialData }
+  />
+)
+
+export const placeHolder = () => (
+  <ChipsInput
+    placeholder={ 'Custom place holder' }
+  />
+)
+
+export const customClass = () => (
+  <ChipsInput
+    className={ styles.microMargin }
+  />
+)
+
+export const getIconHandler = () => {
+  const getChipIcon = chip => {
+    const { label } = chip
+    if(label === 'first') return <EyeEnabled />
+    if(label === 'second') return <ArrowSolidRight />
+    return <Filter />
+  }
+
+  return (
+    <div className={ styles.flexColumn }>
+      <span className={ styles.marginFlexContainer }>
+        Insert <b>first</b> for <b>Eye</b> icon, <b>second</b> for <b>Arrow</b> icon and everything else for <b>Filter</b> icon
+      </span>
+      <ChipsInput
+        getChipIcon={ getChipIcon }
+        onChange={ action('Result') }
+      />
+    </div>
+  )
+}
+
+export const playGround = () => (
+  <ChipsInput
+    onChange={ action('Result') }
+    values={ select('initial values', { noValues: [], withValues: mockInitialData }, []) }
+    placeholder={ text('text', 'Place holder') }
+  />
+)
---
+++ src/components/ChipsInput/index.js
@@ -0,0 +1,1 @@
+export { default as ChipsInput } from './ChipsInput'
