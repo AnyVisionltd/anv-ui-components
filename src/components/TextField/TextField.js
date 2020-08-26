33986d7f250ceef822324d1dfb9cae50ed388b86
illya-anyvision <illyay@anyvision.co>
2020-08-26 13:19:14 +0300

    textfield

---
+++ src/components/TextField/TextField.js
@@ -0,0 +1,198 @@
+import React, { memo, useState, useEffect, useRef } from 'react'
+import propTypes from 'prop-types'
+import classNames from 'classnames'
+import uuid from 'uuid'
+import { ReactComponent as ArrowSolidDown } from '../../assets/svg/ArrowSolidDown.svg'
+import { ReactComponent as ErrorCircleIcon } from '../../assets/svg/ErrorCircleOutlined.svg'
+import { useClickOutsideListener } from '../../hooks'
+import InputBase from '../InputBase'
+import { Menu } from '../Menu/index'
+import styles from './TextField.module.scss'
+
+const types = {
+  options: 'options',
+  text: 'text',
+}
+
+const TextField = React.forwardRef((props, ref) => {
+  const {
+    type,
+    disabled,
+    className,
+    message,
+    error,
+    variant,
+    defaultValue,
+    onClick,
+    items,
+    renderItem,
+    onMenuItemClick,
+    size,
+    placeholder,
+    menuClassName,
+    ...inputProps
+  } = props
+
+  const [value, setValue] = useState(inputProps.value || defaultValue)
+  const [active, setActive] = useState(false)
+  const textFieldRef = ref || useRef({})
+  const inputRef = useRef({})
+
+  const [anchorElement, setAnchorElement] = useState(null)
+
+  const handleMenuClose = () => {
+    setAnchorElement(null)
+    setActive(false)
+  }
+
+  useClickOutsideListener(() => {
+    if(type !== types.options) {
+      setActive(false)
+    }
+  }, textFieldRef)
+
+  useEffect(() => {
+    setValue(inputProps.value || defaultValue)
+  }, [])
+
+  const { id, leadingIcon, onChange, readOnly, multiline } = inputProps
+
+  const onInputChange = e => {
+    const { target: { value } } = e
+    setValue(value)
+    onChange(e)
+  }
+
+  const generateTrailingIcon = () => {
+    if (type === types.options) {
+      return <ArrowSolidDown />
+    }
+    return error ? <ErrorCircleIcon /> : inputProps.trailingComponent
+  }
+
+  const handleClick = e => {
+    if (!disabled && !error && !readOnly) {
+      inputRef.current.focus()
+      setActive(true)
+    }
+    setAnchorElement(anchorElement ? null : textFieldRef.current)
+    onClick(e)
+  }
+
+  const inputId = id || uuid()
+
+  const renderMenu = () => {
+    return (
+      <Menu
+        aria-labelledby={'menu-element'}
+        anchorElement={anchorElement}
+        isOpen={!!anchorElement}
+        onClose={handleMenuClose}
+        className={classNames(styles.textFieldMenu, menuClassName)}
+      >
+        {
+          items.map(item => {
+            return renderItem
+              ?
+              renderItem(item)
+              :
+              (<Menu.Item key={item.value} onClick={onMenuItemClick}>
+                {item.label}
+              </Menu.Item>)
+          })
+        }
+      </Menu>
+    )
+  }
+
+
+  const classes = classNames(
+    styles.TextField,
+    styles[variant],
+    styles[type],
+    styles[size],
+    className,
+    {
+      [styles.error]: error,
+      [styles.disabled]: disabled,
+      [styles.active]: active,
+      [styles.readOnly]: readOnly,
+      [styles.notEmpty]: !!value,
+      [styles.multiline]: multiline,
+    }
+  )
+
+  return (
+    <div className={styles.container}>
+      <div ref={textFieldRef} onClick={handleClick} className={classes}>
+        <label htmlFor={inputId} className={classNames(styles.label, { [styles.left]: !!leadingIcon })}>{placeholder}</label>
+        <InputBase
+          {...inputProps}
+          disabled={disabled}
+          className={classNames(styles.inputBase, { [styles.bottom]: !!placeholder })}
+          id={inputId}
+          value={value}
+          onChange={onInputChange}
+          leadingIconClassName={classNames(styles.leadingIcon)}
+          readOnly={readOnly || type === types.options}
+          trailingComponent={generateTrailingIcon()}
+          ref={inputRef}
+        />
+      </div>
+      {type === types.options && renderMenu()}
+      {message && <span className={classNames(styles.message, { [styles.error]: error })}>{message}</span>}
+    </div>
+  )
+})
+
+TextField.defaultProps = {
+  type: 'text',
+  variant: 'outline',
+  defaultValue: '',
+  size: 'basic',
+  onClick: () => { },
+  onChange: () => { },
+  items: [],
+}
+
+TextField.propTypes = {
+  type: propTypes.string,
+  /** The variant of the textField. */
+  variant: propTypes.oneOf(['fill', 'outline']),
+  /** The default input element value. Use when the component is not controlled. */
+  defaultValue: propTypes.oneOfType([propTypes.string, propTypes.number]),
+  /** Number of rows to display when multiline option is set to true. */
+  rows: propTypes.oneOfType([propTypes.string, propTypes.number]),
+  /** size (height )of the textField */
+  size: propTypes.oneOf(['basic', 'dense']),
+  /** If true, the input will be disabled. */
+  disabled: propTypes.bool,
+  /** Will change the input to text field */
+  multiline: propTypes.bool,
+  /** Will change the input read-only */
+  readOnly: propTypes.bool,
+  /** For css customization. */
+  className: propTypes.string, 
+   /** For css customization. */
+  menuClassName: propTypes.string,
+  /** For icon css customization. */
+  leadingIconClassName: propTypes.string,
+  /** Icon before the children. */
+  leadingIcon: propTypes.element,
+  /** Icon after the children. */
+  trailingComponent: propTypes.element,
+  /** Callback when text change. */
+  onChange: propTypes.func,
+  /** Callback when click. */
+  onClick: propTypes.func,
+  /** Callback when focus. */
+  onFocus: propTypes.func,
+  /** Array of items if type is options. */
+  items: propTypes.arrayOf(propTypes.shape({ value: propTypes.string, label: propTypes.oneOfType([propTypes.string, propTypes.number]) })),
+  /** Callback to render items of type options */
+  renderItem: propTypes.func,  
+  /** Callback to handle item of type options click  */
+  onMenuItemClick: propTypes.func,
+}
+
+export default memo(TextField)
---
+++ src/components/TextField/TextField.test.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { ReactComponent as SvgIcon } from '../../../jest/assets/svgIcon.svg'
+import TextField from './TextField'
+
+describe('<TextField />', () => {
+
+  it('should render leadingIcon', () => {
+    const { container } = render(<TextField leadingIcon={<SvgIcon />} />)
+    const node = container.querySelector('svg')
+    expect(node).toBeTruthy()
+  })
+
+  it('should render trailingComponent', () => {
+    const { container } = render(<TextField trailingComponent={<SvgIcon />} />)
+    const node = container.querySelector('svg')
+    expect(node).toBeTruthy()
+  })
+
+  describe('disabled', () => {
+    it('should NOT be disabled by default', () => {
+      const { getByRole } = render(<TextField />)
+      const node = getByRole('textbox')
+      expect(node.disabled).toBe(false)
+    })
+
+    it('should be disabled when disabled pass', () => {
+      const { getByRole } = render(<TextField disabled />)
+      const node = getByRole('textbox')
+      expect(node).toBeDisabled()
+    })
+  })
+
+  it('should have message', () => {
+    const { getByText } = render(<TextField message={'message'} />)
+    const node = getByText('message')
+    expect(node).toBeTruthy()
+  })
+
+  it('should have placeholder', () => {
+    const { getByText } = render(<TextField placeholder={'my placeholder'} />)
+    const node = getByText('my placeholder')
+    expect(node).toBeTruthy()
+  })
+
+  it('should call onClick', () => {
+    const handleClick = jest.fn()
+    const { getByRole } = render(
+      <TextField onClick={handleClick} />,
+    )
+    const node = getByRole('textbox')
+    fireEvent.click(node)
+    expect(handleClick).toBeCalled()
+  })
+
+  it('should call onChange', () => {
+    const handleOnChange = jest.fn()
+    const { getByRole } = render(
+      <TextField onChange={handleOnChange} />,
+    )
+    const input = getByRole('textbox')
+    fireEvent.change(input, { target: { value: 'I love dogs' } })
+    expect(handleOnChange).toBeCalled()
+    expect(input.value).toBe('I love dogs')
+  })
+})
