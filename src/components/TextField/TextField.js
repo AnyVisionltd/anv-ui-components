3fde7b0cf08481c8742c59712ab563eb0f8795b2
illya-anyvision <illyay@anyvision.co>
2020-08-31 01:33:21 +0300

    fix-textfield-click

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -55,10 +55,8 @@ const TextField = React.forwardRef((props, ref) => {
 
   useEffect(() => {
     window.addEventListener('focusin', setActiveFocus)
-    window.addEventListener('click', setActiveFocus)
     return () => {
       window.removeEventListener('focusin', setActiveFocus)
-      window.removeEventListener('click', setActiveFocus)
     }
   }, [])
 
