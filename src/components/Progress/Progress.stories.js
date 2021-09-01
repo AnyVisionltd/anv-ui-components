775f367dd02eb9861eadd10b9ca65b2b0fbb7443
AdimAnyVision <adi.mizrahi@anyvision.co>
2021-09-01 11:17:55 +0300

    Added more 1 example and small changes

--- src/components/Progress/Progress.stories.js
+++ src/components/Progress/Progress.stories.js
@@ -142,6 +142,79 @@ export const Example = () => {
   )
 }
 
+export const ExampleTiny = () => {
+  const [value, setValue] = useState(0)
+  const [results, setResults] = useState({})
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    if (value >= 100) return
+    const interval = setInterval(() => {
+      setValue(value => value + 1)
+      if (value >= 100) clearInterval(interval)
+    }, 300)
+    return () => clearInterval(interval)
+  }, [value])
+
+  useEffect(() => {
+    if (index > 3) {
+      setTimeout(() => {
+        setResults({})
+        setIndex(0)
+      }, 1500)
+      return
+    }
+    const fetchUser = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(
+          () =>
+            Math.random() > 0.5
+              ? resolve({ success: true, message: 'Fetched successfuly' })
+              : reject({ success: false, message: 'Could not fetch user' }),
+          3000,
+        )
+      })
+    }
+
+    fetchUser()
+      .then(res => setResults({ ...results, [index]: res }))
+      .catch(err => setResults({ ...results, [index]: err }))
+      .finally(() => {
+        setIndex(index + 1)
+        setValue(0)
+      })
+  }, [index])
+
+  const renderTinyProgress = keyIndex => (
+    <Progress
+      key={keyIndex}
+      tiny
+      value={value}
+      withText
+      errorMessage={results[keyIndex]?.message}
+      successMessage={results[keyIndex]?.message}
+      error={results[keyIndex]?.success === false}
+      success={results[keyIndex]?.success === true}
+      inQueue={index < keyIndex}
+      style={{ marginBottom: '16px' }}
+    />
+  )
+
+  const style = {
+    ...containerStyle,
+    justifyContent: 'flexStart',
+    alignItems: 'start',
+  }
+
+  return (
+    <div style={style}>
+      <div style={{ width: '28px' }}>
+        {[...new Array(4)].map((_, index) => renderTinyProgress(index))}
+      </div>
+    </div>
+  )
+}
+
 export const UserFeedbacks = () => (
   <div style={containerStyle}>
     <Progress error />
