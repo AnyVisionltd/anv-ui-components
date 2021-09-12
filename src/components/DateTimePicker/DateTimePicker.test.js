ccf335ae4df38dfb8e9e810501f6a144f2c54aef
orel hindi <orel.hindi@anyvision.co>
2021-09-12 19:31:11 +0300

    ADD tests

---
+++ src/components/DateTimePicker/DateTimePicker.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import moment from 'moment'
+import DateTimePicker from './DateTimePicker'
+
+describe('<DateTimePicker />', () => {
+  it('Should render', () => {
+    const { container } = render(<DateTimePicker />)
+    expect(container).toBeTruthy()
+  })
+
+  it('Should be disabled', () => {
+    const { getByRole } = render(<DateTimePicker disabled />)
+    const node = getByRole('textbox')
+    expect(node.disabled).toBe(true)
+  })
+
+  it('Should call onChange', () => {
+    const handleOnChange = jest.fn()
+    const { getByRole } = render(<DateTimePicker onChange={handleOnChange} />)
+    const input = getByRole('textbox')
+    fireEvent.change(input, { target: { value: moment() } })
+    expect(handleOnChange).toBeCalled()
+    expect(input.value).toBe(moment().format('DD/MM/yyyy HH:mm'))
+  })
+})
