bd4384084b3176bf14efcbf7891cab072b26c53c
AdimAnyVision <adi.mizrahi@anyvision.co>
2021-09-22 16:19:42 +0300

    cahnged hook overflowing

--- src/hooks/UseIsOverflowing/UseIsOverflowing.js
+++ src/hooks/UseIsOverflowing/UseIsOverflowing.js
@@ -4,13 +4,9 @@ function checkOverflow(element) {
   if (!element) {
     return false
   }
-  const curOverflow = element.style.overflow
-  if (!curOverflow || curOverflow === 'visible')
-    element.style.overflow = 'hidden'
   const isOverflowing =
     element.clientWidth < element.scrollWidth ||
     element.clientHeight < element.scrollHeight
-  element.style.overflow = curOverflow
   return isOverflowing
 }
 
