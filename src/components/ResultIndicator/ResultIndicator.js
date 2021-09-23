ab34f4d38dfbe85d2acfb0e07e8ae3f4bb17ffeb
AdimAnyVision <adi.mizrahi@anyvision.co>
2021-09-23 19:05:50 +0300

    changed structure of component

--- src/components/ResultIndicator/ResultIndicator.js
+++ src/components/ResultIndicator/ResultIndicator.js
@@ -9,76 +9,92 @@ import {
 } from '@anyvision/anv-icons'
 import { Tooltip } from '../Tooltip'
 import languageService from '../../services/language'
-import { ReactComponent as StopCircleOutlined } from '../../assets/svg/StopCircleOutlined.svg'
+import { ReactComponent as Somestop } from '../../assets/svg/Union_94.svg'
 import styles from './ResultIndicator.module.scss'
 
 const getTranslation = word => languageService.getTranslation(word)
 
 const ResultIndicator = props => {
-  const {
-    isTiny,
-    error,
-    success,
-    errorMessage,
-    successMessage,
-    inQueue,
-    stopped,
-  } = props
+  const { isTiny, errorMessage, successMessage } = props
   // one big object map, including inqueue
-  const renderInQueue = () => (
-    <Tooltip content={getTranslation('inQueue')}>
-      <div className={styles.inQueueContainer}>
-        <Hourglass /> {!isTiny && getTranslation('inQueue')}
-      </div>
+  // const renderInQueue = () => (
+  //   <Tooltip content={getTranslation('inQueue')}>
+  //     <div className={styles.inQueueContainer}>
+  //       <Hourglass /> {!isTiny && getTranslation('inQueue')}
+  //     </div>
+  //   </Tooltip>
+  // )
+
+  const IndicatorContainer = ({ children, content, className }) => (
+    <Tooltip content={content}>
+      <div className={className}>{children}</div>
     </Tooltip>
   )
 
   const indicatorsMap = {
+    inQueue: (
+      <IndicatorContainer
+        content={successMessage}
+        className={styles.inQueueContainer}
+      >
+        <Hourglass /> {!isTiny && getTranslation('inQueue')}
+      </IndicatorContainer>
+    ),
     success: (
-      <>
+      <IndicatorContainer
+        content={successMessage}
+        className={classNames(styles.resultContainer, styles.success)}
+      >
         <CheckCircleFilled /> {!isTiny && getTranslation('done')}
-      </>
+      </IndicatorContainer>
     ),
     error: (
-      <>
+      <IndicatorContainer
+        content={errorMessage}
+        className={classNames(styles.resultContainer, styles.error)}
+      >
         <TimesCircleFilled /> {!isTiny && getTranslation('failed')}
-      </>
+      </IndicatorContainer>
     ),
     stopped: (
-      <>
-        <StopCircleOutlined /> {!isTiny && getTranslation('stopped')}
-      </>
+      <IndicatorContainer
+        content={getTranslation('stopped')}
+        className={classNames(styles.resultContainer, styles.stopped)}
+      >
+        <Somestop /> {!isTiny && getTranslation('stopped')}
+      </IndicatorContainer>
     ),
   }
 
-  const tooltipContentMap = {
-    success: successMessage,
-    error: errorMessage,
-    stopped: getTranslation('stopped'),
-  }
+  // const tooltipContentMap = {
+  //   success: successMessage,
+  //   error: errorMessage,
+  //   stopped: getTranslation('stopped'),
+  // }
 
-  const determineIndicator = obj => {
-    for (let indicator in obj) {
-      if (props[indicator]) return obj[indicator]
+  const renderIndicator = () => {
+    for (let indicator in indicatorsMap) {
+      if (props[indicator]) return indicatorsMap[indicator]
     }
   }
 
-  const renderResult = () => {
-    const classes = classNames(
-      styles.resultContainer,
-      success && styles.success,
-      error && styles.error,
-      stopped && styles.stopped,
-    )
+  // const renderResult = children => {
+  //   const classes = classNames(
+  //     styles.resultContainer,
+  //     success && styles.success,
+  //     error && styles.error,
+  //     stopped && styles.stopped,
+  //   )
 
-    return (
-      <Tooltip content={determineIndicator(tooltipContentMap)}>
-        <div className={classes}>{determineIndicator(indicatorsMap)}</div>
-      </Tooltip>
-    )
-  }
+  //   return (
+  //     <Tooltip content={determineIndicator(tooltipContentMap)}>
+  //       <div className={classes}>{determineIndicator(indicatorsMap)}</div>
+  //     </Tooltip>
+  //   )
+  // }
 
-  return inQueue ? renderInQueue() : renderResult()
+  // return inQueue ? renderInQueue() : renderResult()
+  return renderIndicator()
 }
 
 ResultIndicator.defaultProps = {
