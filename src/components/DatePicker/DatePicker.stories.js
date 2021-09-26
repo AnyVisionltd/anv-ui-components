ad372bcfb78d9719f3d1de2d588c592704bd0949
AdimAnyVision <adi.mizrahi@anyvision.co>
2021-09-26 11:14:29 +0300

    Chnaged new Date to be fixed and not dynamic so it always throw errors

--- src/components/DatePicker/DatePicker.stories.js
+++ src/components/DatePicker/DatePicker.stories.js
@@ -10,13 +10,12 @@ export default {
 }
 
 export const Basic = () => {
-  const [value, setValue] = useState(moment().format('DD/MM/yyyy'))
+  const defaultDate = new Date(Date.UTC(2021, 8, 23))
+  const [value, setValue] = useState(moment(defaultDate).format('DD/MM/yyyy'))
+
   return (
     <div>
-      <DatePicker
-        onChange={setValue}
-        defaultValue={new Date(Date.UTC(2021, 8, 23))}
-      />
+      <DatePicker onChange={setValue} defaultValue={defaultDate} />
       <p>Selected date: {value}</p>
     </div>
   )
