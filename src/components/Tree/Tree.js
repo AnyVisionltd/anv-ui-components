7dc527620aafcf24fbbaf798817c269eed38a7a9
AdimAnyVision <adi.mizrahi@anyvision.co>
2021-12-16 11:20:28 +0200

    added tooltip, fixed the height of tree and more small change

--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -1,8 +1,14 @@
-import React, { useEffect, useState, useCallback, useImperativeHandle, forwardRef } from 'react'
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  useImperativeHandle,
+  forwardRef,
+} from 'react'
 import classNames from 'classnames'
 import propTypes from 'prop-types'
 import { Search } from '@anyvision/anv-icons'
-import { Checkbox, InputBase } from '../../'
+import { Checkbox, InputBase, Tooltip } from '../../'
 import { CheckboxWithIndeterminateState } from './CheckboxWithIndeterminateState'
 import { VirtualizedTreeList } from './VirtualizedTreeList'
 import { EmptyTreeSearch } from './EmptyTreeSearch'
@@ -16,286 +22,314 @@ import {
   checkAllNodesAreExpanded,
   emptyListTypes,
   ALL_ROOTS_COMBINED_KEY,
-  getNodeParents
+  getNodeParents,
 } from './utils'
 import styles from './Tree.module.scss'
 
 const getTranslation = path => languageService.getTranslation(`${path}`)
 
-const Tree = forwardRef(({
-  nodes,
-  selectedKeys,
-  className,
-  nodesContainerClassName,
-  isSearchable,
-  isBulkActionsEnabled,
-  onSearch,
-  placeholder,
-  onExpand,
-  onSelect,
-  renderLeaf,
-  renderLeafRightSide,
-  displayLabels,
-  rootNodeActions,
-  loadMoreData,
-  maxNestingLevel,
-  noItemsMessage,
-  childrenKey,
-  labelKey,
-  idKey,
-}, ref) => {
-  const [treeInstance, setTreeInstance] = useState(null)
-  const [areAllNodesExpanded, setAreAllNodesExpanded] = useState(false)
-  const [singularNounLabel, pluralNounLabel] = displayLabels
-  const keyValues = { childrenKey, labelKey, idKey }
-
-  const {
-    searchQuery,
-    handleSearch,
-    filteredData,
-    resetSearchData,
-    handleAddNewNodes,
-    handleSetNodeNewProperties
-  } = useTreeVisibleData({
-    initialData: nodes,
-    onSearch,
-    treeInstance,
-    ...keyValues,
-  })
-
-  const {
-    flattenedNodes,
-    setFlattenedNodes,
-    calculateAmountOfSelectedNodesAndChildren,
-    updateAmountOfSelectedNodesAndChildren,
-    handleAddNewFlattenedNodes,
-    handleSetSelectedNodesFromKeysArr
-  } = useFlattenTreeData({
-    data: nodes,
-    selectedKeys,
-    maxNestingLevel,
-    ...keyValues,
-  })
-
-  useImperativeHandle(ref, () => ({
-    nodesMap: Object.freeze({ ...flattenedNodes }),
-    setSelectedKeys: selectedKeysArr => handleSetSelectedNodesFromKeysArr(selectedKeysArr),
-    setNodeProperties: (nodeKey, newProperties) => {
-      const nodePathArr = getNodeParents(nodeKey, flattenedNodes)
-      handleSetNodeNewProperties(nodeKey, newProperties, nodePathArr)
-    }
-  }), [flattenedNodes, handleSetNodeNewProperties, handleSetSelectedNodesFromKeysArr])
-
-  const {
-    totalChildren: totalChildrenInTree,
-    totalSelected: totalSelectedInTree,
-  } = calculateAmountOfSelectedNodesAndChildren(ALL_ROOTS_COMBINED_KEY)
-  const areAllNodesSelected = totalChildrenInTree === totalSelectedInTree
-
-  const handleIsSelected = (node, isCurrentlySelected) => {
-    const newFlattenedNodes = { ...flattenedNodes }
-    const { keys: keysToToggle, nodesMap } = setNodesSelectedStatus({
-      nodesTree: Array.isArray(node) ? node : [node],
-      nodesMap: newFlattenedNodes,
-      isSelected: !isCurrentlySelected,
+const Tree = forwardRef(
+  (
+    {
+      nodes,
+      selectedKeys,
+      className,
+      nodesContainerClassName,
+      isSearchable,
+      isBulkActionsEnabled,
+      onSearch,
+      placeholder,
+      onExpand,
+      onSelect,
+      renderLeaf,
+      renderLeafRightSide,
+      displayLabels,
+      rootNodeActions,
+      loadMoreData,
+      maxNestingLevel,
+      noItemsMessage,
       childrenKey,
+      labelKey,
       idKey,
+    },
+    ref,
+  ) => {
+    const [treeInstance, setTreeInstance] = useState(null)
+    const [areAllNodesExpanded, setAreAllNodesExpanded] = useState(false)
+    const [singularNounLabel, pluralNounLabel] = displayLabels
+    const keyValues = { childrenKey, labelKey, idKey }
+
+    const {
+      searchQuery,
+      handleSearch,
+      filteredData,
+      resetSearchData,
+      handleAddNewNodes,
+      handleSetNodeNewProperties,
+    } = useTreeVisibleData({
+      initialData: nodes,
+      onSearch,
+      treeInstance,
+      ...keyValues,
     })
 
-    setFlattenedNodes(nodesMap)
-    updateAmountOfSelectedNodesAndChildren(
-      Array.isArray(node) ? ALL_ROOTS_COMBINED_KEY : node[idKey],
+    const {
+      flattenedNodes,
+      setFlattenedNodes,
+      calculateAmountOfSelectedNodesAndChildren,
+      updateAmountOfSelectedNodesAndChildren,
+      handleAddNewFlattenedNodes,
+      handleSetSelectedNodesFromKeysArr,
+    } = useFlattenTreeData({
+      data: nodes,
+      selectedKeys,
+      maxNestingLevel,
+      ...keyValues,
+    })
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        get nodesMap() {
+          const { ALL_ROOTS_COMBINED_KEY, ...restNodes } = flattenedNodes
+          return Object.freeze(restNodes)
+        },
+        setSelectedKeys: (keysToAdd, keysToRemove) =>
+          handleSetSelectedNodesFromKeysArr(keysToAdd, keysToRemove),
+        setNodeProperties: (nodeKey, newProperties) => {
+          if (!flattenedNodes[nodeKey]) return
+          const nodePathArr = getNodeParents(nodeKey, flattenedNodes)
+          handleSetNodeNewProperties(nodeKey, newProperties, nodePathArr)
+        },
+      }),
+      [
+        flattenedNodes,
+        handleSetNodeNewProperties,
+        handleSetSelectedNodesFromKeysArr,
+      ],
     )
-    onSelect(keysToToggle)
-  }
 
-  useEffect(() => {
-    if (filteredData.length < nodes.length) {
-      const newAddedNodes = nodes.slice(filteredData.length)
-      handleAddNewNodes(newAddedNodes)
-      handleAddNewFlattenedNodes(newAddedNodes)
-    }
-  }, [filteredData, handleAddNewFlattenedNodes, handleAddNewNodes, nodes])
+    const {
+      totalChildren: totalChildrenInTree,
+      totalSelected: totalSelectedInTree,
+    } = calculateAmountOfSelectedNodesAndChildren(ALL_ROOTS_COMBINED_KEY)
+    const areAllNodesSelected = totalChildrenInTree === totalSelectedInTree
 
-  useEffect(() => {
-    const areAllExpanded = checkAllNodesAreExpanded({
-      nodesTree: filteredData,
-      nodesVirualizedMap: treeInstance?.state?.records,
-      idKey,
-      childrenKey,
-    })
-
-    areAllExpanded !== areAllNodesExpanded &&
-      setAreAllNodesExpanded(areAllExpanded)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [flattenedNodes])
-
-  const handleBulkExpandCollapse = () => {
-    if (treeInstance) {
-      const allExpandedCollapsedNodesObj = setNodesExpandedStatus({
-        nodesTree: filteredData,
-        isOpen: !areAllNodesExpanded,
+    const handleIsSelected = (node, isCurrentlySelected) => {
+      const newFlattenedNodes = { ...flattenedNodes }
+      const { keys: keysToToggle, nodesMap } = setNodesSelectedStatus({
+        nodesTree: Array.isArray(node) ? node : [node],
+        nodesMap: newFlattenedNodes,
+        isSelected: !isCurrentlySelected,
         childrenKey,
         idKey,
       })
-      treeInstance.state.recomputeTree({
-        opennessState: allExpandedCollapsedNodesObj,
-        refreshNodes: true,
-      })
-      setAreAllNodesExpanded(!areAllNodesExpanded)
+
+      setFlattenedNodes(nodesMap)
+      updateAmountOfSelectedNodesAndChildren(
+        Array.isArray(node) ? ALL_ROOTS_COMBINED_KEY : node[idKey],
+      )
+      onSelect(keysToToggle)
     }
-  }
 
-  const handleLoadMoreData = useCallback(async () => {
-    const newNodes = await loadMoreData()
-    if (!newNodes) return
-    handleAddNewNodes(newNodes)
-    handleAddNewFlattenedNodes(newNodes)
-  }, [handleAddNewFlattenedNodes, handleAddNewNodes, loadMoreData])
+    useEffect(() => {
+      if (filteredData.length < nodes.length) {
+        const newAddedNodes = nodes.slice(filteredData.length)
+        handleAddNewNodes(newAddedNodes)
+        handleAddNewFlattenedNodes(newAddedNodes)
+      }
+    }, [filteredData, handleAddNewFlattenedNodes, handleAddNewNodes, nodes])
 
-  const isTreeEmpty = () => {
-    if (!nodes.length) return true
-    if (!searchQuery) return false
-    return !filteredData.some(node => node.visible)
-  }
+    useEffect(() => {
+      const areAllExpanded = checkAllNodesAreExpanded({
+        nodesTree: filteredData,
+        nodesVirualizedMap: treeInstance?.state?.records,
+        idKey,
+        childrenKey,
+      })
 
-  const renderSearchInput = () => (
-    <InputBase
-      placeholder={placeholder}
-      className={styles.searchInput}
-      trailingIcon={<Search />}
-      trailingIconClassName={styles.searchIcon}
-      onChange={handleSearch}
-      value={searchQuery}
-    />
-  )
+      areAllExpanded !== areAllNodesExpanded &&
+        setAreAllNodesExpanded(areAllExpanded)
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [flattenedNodes])
 
-  const renderBulkActions = () => (
-    <div className={styles.bulkActions}>
-      <div className={styles.bulkSelectContainer}>
-        <Checkbox
-          checked={areAllNodesSelected}
-          onChange={() => handleIsSelected(filteredData, areAllNodesSelected)}
-          className={styles.checkbox}
-          id='bulk-select-tree'
-        />
-        <label htmlFor='bulk-select-tree' className={styles.bulkSelectLabel}>
-          {getTranslation(areAllNodesSelected ? 'selectNone' : 'selectAll')}
-        </label>
-      </div>
-      <div className={styles.bulkExpand} onClick={handleBulkExpandCollapse}>
-        {getTranslation(areAllNodesExpanded ? 'collapseAll' : 'expandAll')}
-      </div>
-    </div>
-  )
+    const handleBulkExpandCollapse = () => {
+      if (treeInstance) {
+        const allExpandedCollapsedNodesObj = setNodesExpandedStatus({
+          nodesTree: filteredData,
+          isOpen: !areAllNodesExpanded,
+          childrenKey,
+          idKey,
+        })
+        treeInstance.state.recomputeTree({
+          opennessState: allExpandedCollapsedNodesObj,
+          refreshNodes: true,
+        })
+        setAreAllNodesExpanded(!areAllNodesExpanded)
+      }
+    }
 
-  const renderParentNode = (node, virtualizedListProps, rootNodeProps = {}) => {
-    const { [idKey]: key, [labelKey]: label, [childrenKey]: children } = node
-    const { isOpen, handleExpand } = virtualizedListProps
-    const { renderActions } = rootNodeProps
-    const {
-      totalSelected = 0,
-      totalChildren = children.length,
-    } = calculateAmountOfSelectedNodesAndChildren(key)
-    const isSelected = totalSelected === totalChildren
+    const handleLoadMoreData = useCallback(async () => {
+      const newNodes = await loadMoreData()
+      if (!newNodes) return
+      handleAddNewNodes(newNodes)
+      handleAddNewFlattenedNodes(newNodes)
+    }, [handleAddNewFlattenedNodes, handleAddNewNodes, loadMoreData])
 
-    const infoText = `${totalChildren} ${
-      totalChildren === 1 ? singularNounLabel : pluralNounLabel
-    } | ${totalSelected} ${getTranslation('selected')}`
+    const isTreeEmpty = () => {
+      if (!nodes.length) return true
+      if (!searchQuery) return false
+      return !filteredData.some(node => node.visible)
+    }
 
-    return (
-      <div className={styles.parentNode} key={key}>
-        <Checkbox
-          checked={isSelected}
-          onChange={() => handleIsSelected(node, isSelected)}
-          className={classNames(styles.isSelectedCheckbox, styles.checkbox)}
-        />
-        <p className={styles.parentLabel}>{label}</p>
-        <div className={styles.parentNodeContent}>
-          <div className={styles.parentNodeInfo}>
-            <CheckboxWithIndeterminateState
-              checked={isOpen}
-              onChange={handleExpand}
-              className={styles.checkbox}
-            />
-            <p className={styles.parentInfoText}>{infoText}</p>
-          </div>
+    const renderSearchInput = () => (
+      <InputBase
+        placeholder={placeholder}
+        className={styles.searchInput}
+        trailingIcon={<Search />}
+        trailingIconClassName={styles.searchIcon}
+        onChange={handleSearch}
+        value={searchQuery}
+      />
+    )
+
+    const renderBulkActions = () => (
+      <div className={styles.bulkActions}>
+        <div className={styles.bulkSelectContainer}>
+          <Checkbox
+            checked={areAllNodesSelected}
+            onChange={() => handleIsSelected(filteredData, areAllNodesSelected)}
+            className={styles.checkbox}
+            id='bulk-select-tree'
+          />
+          <label htmlFor='bulk-select-tree' className={styles.bulkSelectLabel}>
+            {getTranslation(areAllNodesSelected ? 'selectNone' : 'selectAll')}
+          </label>
         </div>
-        {!!renderActions && renderActions(node)}
+        <div className={styles.bulkExpand} onClick={handleBulkExpandCollapse}>
+          {getTranslation(areAllNodesExpanded ? 'collapseAll' : 'expandAll')}
+        </div>
       </div>
     )
-  }
 
-  const renderLeafNode = node => {
-    const { [idKey]: key, [labelKey]: label } = node
-    const isSelected = flattenedNodes[key]?.isSelected
+    const renderParentNode = (
+      node,
+      virtualizedListProps,
+      rootNodeProps = {},
+    ) => {
+      const { [idKey]: key, [labelKey]: label, [childrenKey]: children } = node
+      const { isOpen, handleExpand } = virtualizedListProps
+      const { renderActions } = rootNodeProps
+      const {
+        totalSelected = 0,
+        totalChildren = children.length,
+      } = calculateAmountOfSelectedNodesAndChildren(key)
+      const isSelected = !!totalChildren && totalSelected === totalChildren
 
-    return renderLeaf ? (
-      renderLeaf(node)
-    ) : (
-      <div className={styles.leafNode} key={key}>
-        <div className={styles.leftSideLeaf}>
+      const infoText = `${totalChildren} ${
+        totalChildren === 1 ? singularNounLabel : pluralNounLabel
+      } | ${totalSelected} ${getTranslation('selected')}`
+
+      return (
+        <div className={styles.parentNode} key={key}>
           <Checkbox
+            disabled={!totalChildren}
             checked={isSelected}
             onChange={() => handleIsSelected(node, isSelected)}
-            className={styles.checkbox}
+            className={classNames(styles.isSelectedCheckbox, styles.checkbox)}
           />
-          <p className={styles.leafLabel}>{label}</p>
+          <Tooltip content={label} overflowOnly>
+            <p className={styles.parentLabel}>{label}</p>
+          </Tooltip>
+          <div className={styles.parentNodeContent}>
+            <div className={styles.parentNodeInfo}>
+              <CheckboxWithIndeterminateState
+                disabled={!totalChildren}
+                checked={isOpen}
+                onChange={handleExpand}
+                className={styles.checkbox}
+              />
+              <p className={styles.parentInfoText}>{infoText}</p>
+            </div>
+          </div>
+          {!!renderActions && renderActions(node)}
         </div>
-        {renderLeafRightSide && renderLeafRightSide(node)}
-      </div>
-    )
-  }
-
-  const renderNode = (node, layer, virtualizedListProps) => {
-    const { [childrenKey]: children, visible } = node
-    if (!visible) return null
-    if (!children) return renderLeafNode(node)
-    if (layer === 0 && rootNodeActions.length) {
-      return (
-        <RootNode
-          renderNode={rootNodeProps =>
-            renderParentNode(node, virtualizedListProps, rootNodeProps)
-          }
-          menuActions={rootNodeActions}
-        />
       )
     }
-    return renderParentNode(node, virtualizedListProps)
-  }
 
-  const isEmpty = isTreeEmpty()
+    const renderLeafNode = node => {
+      const { [idKey]: key, [labelKey]: label } = node
+      const isSelected = flattenedNodes[key]?.isSelected
 
-  return (
-    <div className={classNames(styles.tree, className)}>
-      {isSearchable && renderSearchInput()}
-      {isBulkActionsEnabled && !isEmpty && renderBulkActions()}
-      <div
-        className={classNames(styles.nodesContainer, nodesContainerClassName)}
-      >
-        {isEmpty && (
-          <EmptyTreeSearch
-            type={
-              nodes.length
-                ? emptyListTypes.NO_RESULTS_FOUND
-                : emptyListTypes.NO_ITEMS_IN_LIST
+      return renderLeaf ? (
+        renderLeaf(node)
+      ) : (
+        <div className={styles.leafNode} key={key}>
+          <div className={styles.leftSideLeaf}>
+            <Checkbox
+              checked={isSelected}
+              onChange={() => handleIsSelected(node, isSelected)}
+              className={styles.checkbox}
+            />
+            <Tooltip content={label} overflowOnly>
+              <p className={styles.leafLabel}>{label}</p>
+            </Tooltip>
+          </div>
+          {renderLeafRightSide && renderLeafRightSide(node)}
+        </div>
+      )
+    }
+
+    const renderNode = (node, layer, virtualizedListProps) => {
+      const { [childrenKey]: children, visible } = node
+      if (!visible) return null
+      if (!children) return renderLeafNode(node)
+      if (layer === 0 && rootNodeActions.length) {
+        return (
+          <RootNode
+            renderNode={rootNodeProps =>
+              renderParentNode(node, virtualizedListProps, rootNodeProps)
             }
-            onClearSearch={resetSearchData}
-            noItemsMessage={noItemsMessage}
+            menuActions={rootNodeActions}
           />
-        )}
-        <VirtualizedTreeList
-          setTreeInstance={setTreeInstance}
-          rootNode={{ ...filteredData }}
-          renderNode={renderNode}
-          loadMoreData={handleLoadMoreData}
-          isSearching={!!searchQuery.length}
-          {...keyValues}
-        />
+        )
+      }
+      return renderParentNode(node, virtualizedListProps)
+    }
+
+    const isEmpty = isTreeEmpty()
+
+    return (
+      <div className={classNames(styles.tree, className)}>
+        {isSearchable && !!nodes.length && renderSearchInput()}
+        {isBulkActionsEnabled && !isEmpty && renderBulkActions()}
+        <div
+          className={classNames(styles.nodesContainer, nodesContainerClassName)}
+        >
+          {isEmpty && (
+            <EmptyTreeSearch
+              type={
+                nodes.length
+                  ? emptyListTypes.NO_RESULTS_FOUND
+                  : emptyListTypes.NO_ITEMS_IN_LIST
+              }
+              onClearSearch={resetSearchData}
+              noItemsMessage={noItemsMessage}
+            />
+          )}
+          <VirtualizedTreeList
+            setTreeInstance={setTreeInstance}
+            rootNode={{ ...filteredData }}
+            renderNode={renderNode}
+            loadMoreData={handleLoadMoreData}
+            isSearching={!!searchQuery.length}
+            {...keyValues}
+          />
+        </div>
       </div>
-    </div>
-  )
-})
+    )
+  },
+)
 
 Tree.defaultProps = {
   nodes: [],
